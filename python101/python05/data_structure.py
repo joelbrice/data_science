ce3b742745190600329d97a65ecfe76fae2f886d
joelbrice <tiogojoel@gmail.com>
2025-01-15 07:02:07 +0100

    forloop done

---
+++ python101/python05/data_structure.py
@@ -0,0 +1,14 @@
+#List
+employees_names =["Jules", "Calvin", "Joel", "Steve", "Mathias", "John", "Peter", "Paul", "James", "Luke", "Mark", "Matthew", "Simon"]
+
+#For loop
+for name in employees_names:
+    #if condition
+    if(name == "Jules"):
+        print(f"Hello {name}, get well soon!")
+    elif(name=="Calvin"):
+        print(f"Hello {name}, you are rocking!")
+    elif(name=="Joel"):
+        print(f"Hello {name}, you are doing great!")
+    else:
+        print("Hello", name)
